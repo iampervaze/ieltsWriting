3c5bcffec3a05a624ac8071319529fa0af5a688a
Pervaze Ahanger <hipervee@gmail.com>
2018-07-11 02:03:56 +0530

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { ToastrModule, GlobalConfig } from 'ngx-toastr';
 import { AppComponent } from './app.component';
+import { appRoutes } from './app.routing';
+
+const toastConfig: Partial<GlobalConfig> = {
+  timeOut: 3000,
+  positionClass: 'toast-top-center',
+  preventDuplicates: true,
+  tapToDismiss: true,
+  closeButton: true,
+  progressAnimation: 'decreasing',
+  progressBar: true
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    CommonModule,
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes, { useHash: true }),
+    ToastrModule.forRoot(toastConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routing.ts
@@ -0,0 +1,9 @@
+import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
+// import { LoginComponent } from './login/login.component';
+// import { GridComponent } from './grid/grid.component';
+export const appRoutes: Routes = [
+    // { path: '', component: LoginComponent },
+    // { path: 'grid', component: GridComponent }
+];
+
