a4334e5b72a2ed13fcdb388df58cdf76ab2e9860
Pervaze Ahanger <hipervee@gmail.com>
2018-07-12 10:54:10 +0530

    changes

--- src/app/components/review/review.component.ts
+++ src/app/components/review/review.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-review',
@@ -9,24 +9,24 @@ export class ReviewComponent {
   config: any = {
     uiColor: '#99000',
     height: '350px',
-    // toolbar: 
+    // toolbar:
     // [
     //     [ 'Source', '-', 'Bold', 'Italic' ]
-    // ],
+    // ]
     font_names: 'Verdana'
   };
-  onChange = (event) => console.log('change');
-  onEditorChange = (event) => console.log('editorChange');
-  onReady = (event) => console.log('ready');
-  onFocus = (event) => console.log('focus');
-  onBlur = (event) => console.log('blur');
-  onContentDom = (event) => console.log('contentDom');
-  onFileUploadRequest = (event) => console.log('fileUploadRequest');
-  onFileUploadResponse = (event) => console.log('fileUploadResponse');
-  onPaste = (event) => console.log('paste');
-  onDrop = (event) => console.log('drop');
-  onSelectionChange = (event) => console.log(event, 'onSelectionChange');
-  onKey = (event) => console.log(event, 'onKey');
+  onChange = event => console.log('change');
+  onEditorChange = event => console.log('editorChange');
+  onReady = event => console.log('ready');
+  onFocus = event => console.log('focus');
+  onBlur = event => console.log('blur');
+  onContentDom = event => console.log('contentDom');
+  onFileUploadRequest = event => console.log('fileUploadRequest');
+  onFileUploadResponse = event => console.log('fileUploadResponse');
+  onPaste = event => console.log('paste');
+  onDrop = event => console.log('drop');
+  onSelectionChange = event => console.log(event, 'onSelectionChange');
+  onKey = event => console.log(event, 'onKey');
 
   editorContent = `<p>Social networking sites, for instance Facebook, are thought by some to have had a detrimental effect on individual people as well as society and local communities. However, in my opinion, while I believe that such sites are mainly beneficial to the individual, I agree that they have had a damaging effect on local communities.</p>
   <p>With regards to individuals, the impact that online social media has had on each individual person has clear advantages. Firstly, people from different countries are brought together through such sites as Facebook whereas before the development of technology and social networking sites, people rarely had the chance to meet or communicate with anyone outside of their immediate circle or community. Secondly, Facebook also has social groups which offer individuals a chance to meet and participate in discussions with people who share common interests.</p>
